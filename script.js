64683186b698d82e3856c4bb582d58a4387d7086
OSNOFA <102733077+osnofAfonso@users.noreply.github.com>
2024-06-05 09:44:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa-principal")
+
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+
+const caixaResultado = document.querySelector(".caixa-resultado")
+
+const textoResultado = document.querySelector(".texto-resultado")
+
+const perguntas = [
+    {
+    enunciado: "Perguntas1"
+    alternativas: ["Alternativa1", "Alternativa2"]
+    },
+    
+    {
+        enunciado: "Perguntas2"
+        alternativas: ["Alternativa1", "Alternativa2"]
+        },
+        
+]
