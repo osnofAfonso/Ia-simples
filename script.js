c6c565bd5de480ebba7600bc49220ae3465006f4
OSNOFA <102733077+osnofAfonso@users.noreply.github.com>
2024-07-03 11:15:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -44,18 +44,28 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
 }
 
-function mostraAlternativas() {
-  
-  for (const alternativa of perguntaAtual.alternativas) const botaoAlternativas = document.createElement("button");
-  caixaAlternativas.appendChild(botãoAlternativas)
-  }
-  
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 }
 
-function mostraPergunta();
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
+}
+
+mostraPergunta();
